3d318439679167f2f0195030aeb577cdcc96f9e1
prasant-sen <prasantsen1986@gmail.com>
2024-08-24 16:17:05 +0530

    1st Commit

---
+++ src/pages/Chat/Chat.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Chat.css'
+const Chat = () => {
+  return (
+    <div>
+        We Are on the Chat Page
+    </div>
+  )
+}
+
+export default Chat
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Login.css'
+import asstes from '../../assets/assets'
+import assets from '../../assets/assets'
+
+const Login = () => {
+  return (
+    <div className='login'>
+        <img src={assets.logo_big} alt="" className="logo" />
+        
+    </div>
+  )
+}
+
+export default Login
